7c3dd47b993aa66e0d7eb5b386abea344168dcd4
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-21 19:16:45 +0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,81 +5,89 @@ import csv
 import os.path
 
 class Tasks:
-    def __init__(self, master):
-        self.master = master
+def __init__(self, master):
+self.master = master
 
-        # Создание виджетов
-        self.title_label = tk.Label(master, text="Tasks", font=("Arial", 16, "bold"))
+# Создание виджетов
+self.title_label = tk.Label(master, text="Tasks", font=("Arial", 16, "bold"))
 
-        # Размещение виджетов на экране
-        self.title_label.pack(pady=20)
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
 
 class Accounts:
-    def __init__(self, master):
-        self.master = master
+def init(self, master):
+self.master = master
 
-        # Создание виджетов
-        self.title_label = tk.Label(master, text="Accounts", font=("Arial", 16, "bold"))
+# Создание виджетов
+self.title_label = tk.Label(master, text="Accounts", font=("Arial", 16, "bold"))
 
-        # Размещение виджетов на экране
-        self.title_label.pack(pady=20)
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
 
 class Settings:
-    def __init__(self, master):
-        self.master = master
+def init(self, master):
+self.master = master
 
-        # Создание виджетов
-        self.title_label = tk.Label(master, text="Settings", font=("Arial", 16, "bold"))
+# Создание виджетов
+self.title_label = tk.Label(master, text="Settings", font=("Arial", 16, "bold"))
 
-        # Размещение виджетов на экране
-        self.title_label.pack(pady=20)
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
 
+class PolyWalletGenerator:
+def init(self, master):
+self.master = master
+
+# Создание виджетов
+self.title_label = tk.Label(master, text="Poly Wallet Generator", font=("Arial", 16, "bold"))
+
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
+
+class PolyTokenManager:
+def init(self, master):
+self.master = master
+
+# Создание виджетов
+self.title_label = tk.Label(master, text="Poly Token Manager", font=("Arial", 16, "bold"))
+
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
 
 class RPC:
+def init(self, master):
+self.master = master
+
+# Создание виджетов
+self.title_label = tk.Label(master, text="RPC", font=("Arial", 16, "bold"))
+
+# Размещение виджетов на экране
+self.title_label.pack(pady=20)
+
+class WalletManager:
     def __init__(self, master):
         self.master = master
 
         # Создание виджетов
-        self.title_label = tk.Label(master, text="RPC", font=("Arial", 16, "bold"))
+        self.title_label = tk.Label(master, text="Wallet Manager", font=("Arial", 16, "bold"))
+        self.wallets_button = tk.Button(master, text="Poly Wallet Generator", font=("Arial", 12), command=self.poly_wallet_generator)
+        self.tokens_button = tk.Button(master, text="Poly Token Manager", font=("Arial", 12), command=self.poly_token_manager)
+
+        # Размещение виджетов на экране
+        self.title_label.pack(pady=20)
+        self.wallets_button.pack(pady=10)
+        self.tokens_button.pack(pady=10)
+
+    def poly_wallet_generator(self):
+        # Создание виджетов
+        self.title_label = tk.Label(self.master, text="Poly Wallet Generator", font=("Arial", 16, "bold"))
 
         # Размещение виджетов на экране
         self.title_label.pack(pady=20)
 
-class WalletGeneratorGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Polygon Wallet Generator")
-
-        # Определение размеров окна
-        screen_width = master.winfo_screenwidth()
-        screen_height = master.winfo_screenheight()
-        window_width = int(screen_width * 3 / 4)
-        window_height = int(screen_height * 3 / 4)
-        x = (screen_width - window_width) // 2
-        y = (screen_height - window_height) // 2
-        master.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
+    def poly_token_manager(self):
         # Создание виджетов
-        self.left_frame = tk.Frame(master, bg="#f8f8f8")
-        self.right_frame = tk.Frame(master)
-
-        # Создание модулей
-        self.tasks = Tasks(self.left_frame)
-        self.accounts = Accounts(self.left_frame)
-        self.settings = Settings(self.left_frame)
-        self.rpc = RPC(self.left_frame)
+        self.title_label = tk.Label(self.master, text="Poly Token Manager", font=("Arial", 16, "bold"))
 
         # Размещение виджетов на экране
-        self.left_frame.pack(side=tk.LEFT, fill=tk.Y, padx=(10, 0))
-        self.right_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-        # Визуальное отделение левой и правой колонок
-        separator = ttk.Separator(self.master, orient="vertical")
-        separator.place(relx=0.2, rely=0, relheight=1)
-        label = tk.Label(self.master, text="Polygon Wallet Generator", font=("Arial", 16, "bold"))
-        label.place(relx=0.25, rely=0.02)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    gui = WalletGeneratorGUI(root)
-    root.mainloop()
+        self.title_label.pack(pady=20)
