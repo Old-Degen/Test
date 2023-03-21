9942d4f96ca7a2625b0e693ecaca4a926a4547a9
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-21 20:13:37 +0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
-
+from modules.tasks import Tasks
 from modules.accounts import Accounts
 from modules.settings import Settings
 from modules.rpc import RPC
+from modules.poly_wallet_generator import PolyWalletGenerator
+from modules.poly_token_manager import PolyTokenManager
+
 
 class WalletGeneratorGUI:
     def __init__(self, master):
@@ -28,6 +31,8 @@ class WalletGeneratorGUI:
         self.accounts = Accounts(self.left_frame)
         self.settings = Settings(self.left_frame)
         self.rpc = RPC(self.left_frame)
+        self.poly_wallet_generator = PolyWalletGenerator(self.left_frame)
+        self.poly_token_manager = PolyTokenManager(self.left_frame)
 
         # Размещение виджетов на экране
         self.left_frame.pack(side=tk.LEFT, fill=tk.Y, padx=(10, 0))
@@ -37,7 +42,7 @@ class WalletGeneratorGUI:
         separator = ttk.Separator(self.master, orient="vertical")
         separator.place(relx=0.2, rely=0, relheight=1)
         label = tk.Label(self.master, text="Polygon Wallet Generator", font=("Arial", 16, "bold"))
-        label.place(relx=0.25, rely=0.02)
+        label.place(relx=0.25, rely=0.02))
 
 
 if __name__ == "__main__":
