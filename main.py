9ae8e632f30de20656762e34cb35dd999dc3f040
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-21 20:10:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import messagebox
 from web3 import Web3
 from gui import WalletGeneratorGUI
 from csv_helper import add_to_csv
+dirname = os.path.dirname(__file__)
+csv_path = os.path.join(dirname, 'private', 'wallets.csv')
+csv_helper_path = os.path.join(dirname, 'csv_helper.py')
 
 def main():
     root = tk.Tk()
