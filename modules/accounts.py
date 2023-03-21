22086746839fb58a118443a450f7ee420ee7d9a1
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-21 23:42:23 +0400

    1
    1

--- modules/accounts.py
+++
@@ -1,6 +0,0 @@
-class Accounts:
-    def __init__(self):
-        self.accounts = []
-
-    def add_account(self, group, name, address, private_key):
-        self.accounts.append({'Group': group, 'Name': name, 'Address': address, 'Private Key': private_key})
