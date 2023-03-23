27259a86854a58694971e8411885f42d88821b2c
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-23 22:04:56 +0400

    Update nft_manager.py

--- modules/nft_manager.py
+++ modules/nft_manager.py
@@ -1,5 +1,7 @@
 from web3 import Web3, HTTPProvider
 from web3.contract import Contract
+from modules.constants import Constants
+from modules.wallet_manager import WalletManager
 
 
 class NFTManager:
@@ -14,3 +16,22 @@ class NFTManager:
         contract = self.web3.eth.contract(address=contract_address, abi=abi)
         return contract
     # Другие методы для работы с контрактом NFT...
+
+    def load_nft():
+        # Load constants
+        constants = Constants()
+
+        # Get NFT contract address and ABI from constants
+        nft_address = constants.NFT_CONTRACT_ADDRESS
+        NFT_ABI = constants.NFT_ABI
+
+        # Initialize Web3 and NFTManager
+        web3 = Web3(HTTPProvider(constants.PROVIDER_URI))
+        rpc = RPC(web3)
+        nft_manager = NFTManager(nft_address, NFT_ABI, rpc)
+
+        # Get WalletManager instance
+        sender_address = constants.SENDER_ADDRESS
+        wallet_manager = WalletManager(sender_address, web3)
+
+        return nft_manager, wallet_manager
