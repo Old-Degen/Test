ba2819083bb9d578096f59ba8aa0d129026b0b78
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-23 22:28:28 +0400

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,8 +1,19 @@
-# Импорты
+
 from typing import Dict, Any
+import json
 
 # Класс с константами
 class Constants:
+    def __init__(self):
+        self.load_tokens()
+
+    def load_tokens(self):
+        with open('ABI/matic.json', 'r') as f:
+            self.TOKEN_ABI = json.load(f)
+
+
+
+
     # Адреса смарт-контрактов
     NFT_CONTRACT_ADDRESS = "0x1234567890abcdef"
     TOKEN_CONTRACT_ADDRESS = "0xfedcba0987654321"
@@ -21,3 +32,8 @@ class Constants:
     @classmethod
     def get_some_value(cls) -> str:
         return cls.SOME_CONSTANT
+
+    with open('ABI/matic.json', 'r') as f:
+        TOKEN_ABI = json.load(f)
+
+    TOKEN_CONTRACT_ADDRESS = "0xfedcba0987654321"
